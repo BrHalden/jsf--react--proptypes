7c2dd089fa54a97636654d872c04b544d5f8191d
Kristoffer Bråthen Halden <>
2020-03-17 15:55:37 +0100

    Done. This task was very helpful in understanding this concept for me

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 // import component
 import ExampleComponent from './ExampleComponent.js';
+import Heading from './Heading.js';
+import Image from './Image.js';
+import Repeater from './Repeater.js';
+import ShowHide from './ShowHide.js';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Noroff Task</h1>
+      <Heading heading="This is heading">
 
+      </Heading>
+      <Repeater count={3}>
+        <p>This is the child node</p>
+      </Repeater>
+      <Image source="https://placedog.net/250/250" alternativeText="Dog Pic">
+
+      </Image>
+      <ShowHide show={true}>
+        <p>Show hide content node(shown)</p>
+      </ShowHide>
+      <ShowHide show={false}>
+        <p>Show hide content node(hidden)</p>
+      </ShowHide>
       {/* EXAMPLE with props the validate */}
       <ExampleComponent
       // we used "" instead of {} to pass in strings
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Heading(props) {
   return (
-    <h1 className="Heading">{props.text}</h1>
+    <h1 className="Heading">{props.heading}</h1>
   );
 }
+
+Heading.propTypes = {
+  heading: PropTypes.string.isRequired
+};
